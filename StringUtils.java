ca0350ea1cdcd8fa99666e1d3a2465786dcc7d79
YiFei Wang <beiifeng@hotmail.com>
2018-11-13 11:25:54 +0800

    Update StringUtils.java

--- StringUtils.java
+++ StringUtils.java
@@ -1,9 +1,16 @@
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @author Eephaie
+ * @version 1.0.0.0
+ * @date 2018/11/13 10:57
+ */
 public class StringUtils {
 
     public static void main(String[] args) {
         String s = "测a试b字c符d串e";
-        int len = 4;
-        String charsetName = "utf-8";
+        int len = 9;
+        String charsetName = "utf8";
         System.out.println(subStringb(s, len, charsetName));
     }
 
@@ -11,52 +18,69 @@ public class StringUtils {
      * 按字节长度截取字符串
      *
      * @param orgin       需要截取的字符串
-     * @param length      需要保留的字节长度
+     * @param blength     需要保留的字节长度
      * @param charsetName 编码
      * @return 截取后的字符串
      */
-    public static String subStringb(String orgin, int length, String charsetName) {
+    public static String subStringb(String orgin, int blength, String charsetName) {
 
         String result = "";
+        byte[] utf16be = new byte[]{(byte) 0xFE, (byte) 0xFF};
+        byte[] utf16le = new byte[]{(byte) 0xFF, (byte) 0xFE};
         try {
             byte[] bs = orgin.getBytes(charsetName);
-            if (length >= bs.length) {
+            if (blength >= bs.length) {
                 return orgin;
             }
             if ("UTF8".equals(charsetName.toUpperCase()) || "UTF-8".equals(charsetName.toUpperCase())) {
-
                 // utf8 encoding
-                // 0000 - 007F 0xxxxxxx
-                // 0080 - 07FF 110xxxxx 10xxxxxx
-                // 0800 - FFFF 1110xxxx 10xxxxxx 10xxxxxx
-                while (length > 0) {
-                    if ((bs[length] | 0x7F) == 0x7F) {
+                // 0000 0000 - 0000 007F 0xxxxxxx
+                // 0000 0080 - 0000 07FF 110xxxxx 10xxxxxx
+                // 0000 0800 - 0000 FFFF 1110xxxx 10xxxxxx 10xxxxxx
+                // 0001 0000 - 0010 FFFF 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+                while (blength > 0) {
+                    if ((bs[blength] | 0x7F) == 0x7F) {
                         break;
                     }
-                    if ((bs[length] & 0xC0) == 0xC0) {
+                    if ((bs[blength] & 0xC0) == 0xC0) {
                         break;
                     }
-                    if ((bs[length] & 0xE0) == 0xE0) {
+                    if ((bs[blength] & 0xE0) == 0xE0) {
                         break;
                     }
-                    length--;
+                    if ((bs[blength] & 0xF0) == 0xF0) {
+                        break;
+                    }
+                    blength--;
                 }
-            } else if ("GBK".equals(charsetName.toUpperCase()) || "GBK".equals(charsetName.toUpperCase())) {
-                boolean removLast = length % 2 == 1;
-                for (int i = 0; i < length; i++) {
+            } else if ("GBK".equals(charsetName.toUpperCase())) {
+                boolean removLast = blength % 2 == 1;
+                for (int i = 0; i < blength; i++) {
                     if ((bs[i] | 0x7F) == 0x7F) {
                         removLast = !removLast;
                     }
                 }
                 if (removLast) {
-                    length--;
+                    blength--;
                 }
             } else if ("UNICODE".equals(charsetName.toUpperCase())) {
-                length += 2;
+                if (blength % 2 == 1) {
+                    blength--;
+                }
             } else if ("UTF-16".equals(charsetName.toUpperCase()) || "UTF16".equals(charsetName.toUpperCase())) {
-                length += 2;
+                if (blength % 2 == 1) {
+                    blength--;
+                }
+            } else if ("UTF-16BE".equals(charsetName.toUpperCase())) {
+                if (blength % 2 == 1) {
+                    blength--;
+                }
+            } else if ("UTF-16LE".equals(charsetName.toUpperCase())) {
+                if (blength % 2 == 1) {
+                    blength--;
+                }
             }
-            result = new String(bs, 0, length, charsetName);
+            result = new String(bs, 0, blength, charsetName);
         } catch (UnsupportedEncodingException ex) {
             ex.printStackTrace();
         }
